731c565c4be70b82617882636e43ac88fd46f9a7
Martin Gottweis <gottwik@gmail.com>
2016-05-22 19:54:27 +0200

    multiculture admin ui support

--- cms/.users.js
+++ cms/.users.js
@@ -6,17 +6,5 @@
 			hash: 'a271e2b37fe441a14be469168367a435f0c4d84f6ec9f8b1fa7cded9566466f9',
 			user_created_timestamp: 1463555604417
 		},
-		{
-			username: 'peer',
-			salt: 'c7e3dc36249072a16701a7e82add48ca',
-			hash: '5ed1ef39f7aa560fb1bfdb289cfff830d290f76be3039c92586ecd2c7fe75513',
-			user_created_timestamp: 1463657564244
-		},
-		{
-			username: 'test',
-			salt: '30ce633713bb94ec009da9896da59320',
-			hash: 'd695cd783ed72bf677b4c613679953cd86e78cc5032b205633b739d10f3f697c',
-			user_created_timestamp: 1463724054085
-		}
 	]
 }
--- cms/index.js
+++ cms/index.js
@@ -1,51 +1,20 @@
 {
 	$page_name: 'Homepage',
 	greeting: 'dude',
+	$greeting_de: 'schnitzel',
 	superlative: '22222',
-	panel: {
-		title: 'Panel title',
-		subtitle: 'subtitle'
-	},
-	product_list: [
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Hobbes',
-			price: 123
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Hobbes',
-			price: 123
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Hobbes',
-			price: 123
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		},
-		{
-			product_name: 'Calvin',
-			price: 45
-		}
-	]
+	// panel: {
+	// 	title: 'Panel title',
+	// 	subtitle: 'subtitle'
+	// },
+	// product_list: [
+	// 	{
+	// 		product_name: 'Calvin',
+	// 		price: 45
+	// 	},
+	// 	{
+	// 		product_name: 'Hobbes',
+	// 		price: 123
+	// 	},
+	// ]
 }
