4ca16f0de405a83f627bf138103f602217fc80b4
Martin Gottweis <gottwik@gmail.com>
2016-05-23 21:52:14 +0200

    cleaned up

--- cms/global/global.js
+++ cms/global/global.js
@@ -1,3 +1,15 @@
 {
-	company_name: 'something'
+	$imag_e_label: 'Background image',
+	$imag_e_type: 'image',
+	imag_e: 'http://vignette3.wikia.nocookie.net/candh/images/7/70/Calvin%26HobbesDartWarGames.jpg/revision/latest?cb=20140223090538',
+	products: {
+		ninjago: {
+			label: 'ninjago',
+			price: 123
+		},
+		nexo_knights: {
+			label: 'nexo_knights',
+			price: 200
+		}
+	}
 }
