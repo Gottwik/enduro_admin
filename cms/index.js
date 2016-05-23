95f605601fb9ceafbd5efa618a3f49e08b77a410
Martin Gottweis <gottwik@gmail.com>
2016-05-23 09:56:02 +0200

    image upload finished

--- cms/index.js
+++ cms/index.js
@@ -3,19 +3,5 @@
 	greeting: 'dude',
 	$greeting_de: 'schnitzel',
 	$image_type: 'img',
-	image: 'http://vignette3.wikia.nocookie.net/candh/images/7/70/Calvin%26HobbesDartWarGames.jpg/revision/latest?cb=20140223090538',
-	// panel: {
-	// 	title: 'Panel title',
-	// 	subtitle: 'subtitle'
-	// },
-	// product_list: [
-	// 	{
-	// 		product_name: 'Calvin',
-	// 		price: 45
-	// 	},
-	// 	{
-	// 		product_name: 'Hobbes',
-	// 		price: 123
-	// 	},
-	// ]
+	image: '/assets/img/uploaded/Screen Shot 2016-05-09 at 16.53.27.png'
 }
