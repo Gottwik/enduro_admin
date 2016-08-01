d3b03dd16f06c8069a3ae95989279d3af46594a8
Martin Gottweis <gottwik@gmail.com>
2016-08-01 13:44:57 +0200

    added temp button wip

--- cms/.settings.js
+++ cms/.settings.js
@@ -1,6 +1,7 @@
 {
 	settings: {
 		$admin_background_image_type: 'image',
-		admin_background_image: '/assets/img/uploaded/1468939750_9xwb6yug8iqo7zl0daemi.png'
+		admin_background_image: '/assets/img/uploaded/1468939750_9xwb6yug8iqo7zl0daemi.png',
+		dark: true
 	}
 }
