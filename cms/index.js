c5b6928061eb51bc06ceeff29744063037b8290c
Martin Gottweis <gottwik@gmail.com>
2016-09-01 08:41:16 +0200

    fixed overlapping object in array control

--- cms/index.js
+++ cms/index.js
@@ -1,51 +1,14 @@
 {
-	$slides_min: 1,
-	$slides_max: 5,
-	slides: [
+	events: [
 		{
-			$image_type: 'image',
-			image: 'https://static.pexels.com/photos/5317/food-salad-restaurant-person-large.jpg'
-		},
-		{
-			$image_type: 'image',
-			image: 'https://static.pexels.com/photos/41123/pexels-photo-41123-large.jpeg'
-		},
-		{
-			$image_type: 'image',
-			image: 'https://static.pexels.com/photos/62097/pexels-photo-62097-large.jpeg'
+			label: 'Australia',
+			specs: {
+				riders_registered: '95',
+				race_distance: '95',
+				highest_point: 'pico mawson',
+				speed_limit: '120 kms/h'
+			},
+
 		}
-	],
-	$tested_foods_min: 3,
-	$tested_foods_max: 3,
-	tested_foods: [
-		{
-			$icon_type: 'fa',
-			icon: 'music',
-			title: 'Hot dogsss',
-			$text_type: 'textarea',
-			text: 'We will test your hotdogs. Additional services include determining optimal condiment ratios.'
-		},
-		{
-			$icon_type: 'fa',
-			icon: 'heart',
-			title: 'Hamburgers',
-			$text_type: 'textarea',
-			text: 'We will test your hotdogs. Additional services include determining optimal condiment ratios.'
-		},
-		{
-			$icon_type: 'fa',
-			icon: 'star',
-			title: 'Gourmet foods',
-			$text_type: 'textarea',
-			text: 'We will test your hotdogs. Additional services include determining optimal condiment ratios.'
-		}
-	],
-	testimonial: {
-		$image_size: 6,
-		$image_type: 'image',
-		image: 'https://static.pexels.com/photos/51969/model-female-girl-beautiful-51969-large.jpeg',
-		$text_size: 6,
-		$text_type: 'textarea',
-		text: 'I ordered food tasting services from the Boutique food tasters for my wedding. They tasted everything, some items even multiple times.'
-	}
+	]
 }
