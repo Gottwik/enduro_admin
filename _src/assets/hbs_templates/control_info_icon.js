aa386be74490b68d3aec330a9a16418c81591a1e
Martin Gottweis <martin.gottweis@booking.com>
2017-06-22 15:24:23 +0200

    migrated to _generated

--- _src/assets/hbs_templates/control_info_icon.js
+++
@@ -1,3 +0,0 @@
-define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<i ng-if=\"terminated_context.info\" ngtooltipster class=\"fa fa-info-circle tooltip control-tooltip\" title=\"{{terminated_context.info}}\"></i>";
-},"useData":true}); });
