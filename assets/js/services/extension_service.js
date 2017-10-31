a00f612ef406139f05c632db3029b57c4c439cdf
Martin Gottweis <martin.gottweis@booking.com>
2017-10-31 01:45:35 +0100

    removed admin extension logic to allow brick injects

--- assets/js/services/extension_service.js
+++ assets/js/services/extension_service.js
@@ -7,17 +7,13 @@ enduro_admin_app.factory('extension_service', ['$http', 'url_config', '$cookies'
 				if (res.data && res.data.success) {
 					for (r in res.data.data) {
 						$('<script>')
-							.attr('src', get_link_to_extension(res.data.data[r]))
+							.attr('src', res.data.data[r])
 							.appendTo('head')
 					}
 				} else {
 					console.log('error getting extension list')
 				}
 			}, user_service.error)
-	}
-
-	function get_link_to_extension (extension) {
-		return '/assets/admin_extensions/' + extension
 	}
 
 	return extension_service
